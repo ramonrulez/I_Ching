38a2c6c06aa9bfb3f28de69e1e564c9d13bce8ac
ramonrulez <papageogeo91@gmail.com>
2024-08-20 23:00:01 +0300

    Intro print

--- i_ching.cpp
+++ i_ching.cpp
@@ -1,9 +1,40 @@
- #include "Hexagram.h"
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <string>
 #include <iostream>
+
+namespace TextLocation
+{
+    std::string bookFolder{"I_Ching_book/Book_in_parts/"};
+    std::string intro{"0_intro.txt"}; 
+}
+
+void printIntro()
+{
+    std::string inputString;
+    std::ifstream inFile;
+
+    inFile.open(TextLocation::bookFolder + TextLocation::intro);
+    
+    if (!inFile)
+    {
+        std::cout << "Unable to open file\n";
+        exit(1);
+    }
+    
+    while(std::getline(inFile, inputString)){
+        std::cout << inputString << '\n';
+    }
+    
+    inFile.close();
+}   
+
+//  MAIN-----------------------------------------------------------------------
 
 int main() {
     
-    
+    printIntro();
 
     return 0;
 }
